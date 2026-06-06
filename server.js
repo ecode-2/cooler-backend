dd66b498194ab318b219797620d200cb02b2d6f9
ecode-2 <elijahcoffer1@msn.com>
2026-06-06 17:42:57 -0500

    Update server.js
    Shopify test

--- server.js
+++ server.js
@@ -6,18 +6,44 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const SHOPIFY_STORE = process.env.SHOPIFY_STORE;
+const SHOPIFY_ADMIN_TOKEN = process.env.SHOPIFY_ADMIN_TOKEN;
+
 app.get('/', (req, res) => {
 res.send('Cooler Backend Running');
 });
 
-app.post('/create-order', (req, res) => {
+app.get('/test-shopify', async (req, res) => {
 
-console.log(req.body);
+try {
 
-res.json({
-success: true,
-message: 'Create order route works'
+```
+const response = await fetch(
+  `https://${SHOPIFY_STORE}/admin/api/2025-01/shop.json`,
+  {
+    headers: {
+      'X-Shopify-Access-Token': SHOPIFY_ADMIN_TOKEN,
+      'Content-Type': 'application/json'
+    }
+  }
+);
+
+const data = await response.json();
+
+res.json(data);
+```
+
+} catch (error) {
+
+```
+console.error(error);
+
+res.status(500).json({
+  error: error.message
 });
+```
+
+}
 
 });
 
